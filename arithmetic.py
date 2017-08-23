a20a8c18dc72075198246cd223c913240d6bd1b7
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-23 12:03:53 -0700

    divide function working

--- arithmetic.py
+++ arithmetic.py
@@ -22,6 +22,7 @@ def multiply(num1, num2):
 def divide(num1, num2):
     """Divide the first input by the second, returning a floating point."""
 
+    return num1 / num2
 
 def square(num1):
     """Return the square of the input."""
